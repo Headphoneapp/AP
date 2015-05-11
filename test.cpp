13ab1790e82e4a34af6a0e7c7e866941e6200d1a
HamidRS <hamid@Hamids-MacBook-Pro.local>
2015-05-11 13:57:28 +0430

    1

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string s = "salamsalam";
+	string t = s.substr(0, 5);
+	cout << "hi" << endl;
+	cout << t << endl;
+//s
+}
